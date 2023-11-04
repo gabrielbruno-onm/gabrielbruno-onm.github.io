9970afc5a2bf95c2931e7639ef1f8446f08657a7
gabrielbruno-onm <77846271+gabrielbruno-onm@users.noreply.github.com>
2023-11-04 00:14:53 -0300

    Atualizações noturnas
    Update no botão. Update na segurança das variáveis. Update que permite trabalhar caso o usuário digite o e-mail com letras maiúsculas (converterá para lowercase). Já vai passar o e-mail pré-populado no checkout.

--- redirect/script.js
+++ redirect/script.js
@@ -1,64 +1,81 @@
+var inputEmail = window.document.querySelector('#inputEmail');
 var inputBtn = window.document.querySelector('#submitButton');
 var loaderBtn = window.document.querySelector('.lds-ring')
+
+
+//Função para mudar a estética do form enquanto faz consulta de API
+function aestheticOn() {
+    inputBtn.style.background = '#0b7cac';
+    inputBtn.value = 'Carregando...';
+    inputBtn.style.boxShadow = 'none';
+    loaderBtn.style.display = "inline-block";
+}
+
+//Função para voltar a estética ao normal após consulta de API
+function aestheticOff() {
+    inputBtn.style.background = '#dd004a';
+    inputBtn.value = 'Confirmar';
+    inputBtn.style.boxShadow = 'inherit';
+    loaderBtn.style.display = "none";
+}
 
 function consultar() {
 
     //Armazenando email:
-    var userEmail = window.document.querySelector('#inputemail');
-    var userEmail = userEmail.value;
+    var userEmail = window.document.querySelector('#inputEmail').value.toLowerCase();
 
     //Verificando se o e-mail é um e-mail válido:
     var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
     if (userEmail.match(validRegex)) {
-        //alert(`${userEmail} é um e-mail válido!`);
+
+        //Fazendo email virar JSON
+        const payload = `{"email": "${userEmail}"}`;
+        
+        const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Connection': 'keep-alive'
+        },
+        body: payload,
+        };
+
+        //Usando o e-mail para consultar na API:
+        fetch('https://hook.us1.make.com/xueb5gx1yufx3v7gr6km55v47vkc4bwy', options)
+        .then(response => {
+        return response.json();
+        })
+        .then(data => {
+            var offer_code = data.offer_code;
+            var errorMsg = data.error;
+            console.log("Fetch");
+            
+            //Redirect
+            switch (offer_code) {
+                case 'jwn1nzud':
+                    urlHotmart = `https://pay.hotmart.com/?off=jwn1nzud&offDiscount=INVITATION12&sck=checkout1&email=${encodeURIComponent(userEmail)}`;
+                    window.location.href = urlHotmart;
+                    break;
+                case 'qii5bwet':
+                    urlHotmart = `https://pay.hotmart.com/?off=jwn1nzud&offDiscount=INVITATION12&sck=checkout2&email=${encodeURIComponent(userEmail)}`;
+                    window.location.href = urlHotmart;
+                    break;
+                default:
+                    alert('Este email não está na lista de assinantes elegíveis para a oferta. Por favor, tente com outro e-mail.');
+                    console.log("Executei.");
+                    aestheticOff();
+                break;
+            }
+        });
 
     } else {
-        alert(`O e-mail preenchido é inválido. Por favor, tente novamente.`);
-        inputBtn.style.background = '#dd004a';
-        loaderBtn.style.display = "none";
+        alert(`O texto digitado não é um e-mail. Por favor, tente novamente.`);
+        aestheticOff();
     }
-
-    //Fazendo email virar JSON
-    const payload = `{"email": "${userEmail}"}`;
-    
-    const options = {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'Connection': 'keep-alive'
-    },
-    body: payload,
-    };
-
-    //Usando o e-mail para consultar na API:
-    fetch('https://hook.us1.make.com/xueb5gx1yufx3v7gr6km55v47vkc4bwy', options)
-    .then(response => {
-      return response.json();  
-    })
-    .then(data => {
-        var offer_code = data.offer_code;
-        
-        //Redirect
-        switch (offer_code) {
-            case 'jwn1nzud':
-                window.location.href = "https://pay.hotmart.com/?off=jwn1nzud&offDiscount=INVITATION12&sck=checkout1";
-                break;
-            case 'qii5bwet':
-                window.location.href = "https://pay.hotmart.com/?off=jwn1nzud&offDiscount=INVITATION12&sck=checkout2";
-                break;
-        }
-    });
 }
-
-function aesthetic() {
-    inputBtn.style.background = '#0b7cac';
-    loaderBtn.style.display = "inline-block";
-}
-
-
 inputBtn.addEventListener('click', function() {
-    aesthetic();
+    aestheticOn();
     consultar();
 });
